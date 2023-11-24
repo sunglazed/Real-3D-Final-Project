f2b70a2f716594bcdaea69c787c29531357d63ae
efetman <efetman@terpmail.umd.edu>
2023-11-24 15:34:25 -0500

    materials and "trigger" ish

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-    public Animator myDoor = null;
-    public bool openTrigger = false;
-    public bool closeTrigger = false;
+    [SerializeField] private Animator myDoor = null;
+   [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closeTrigger = false;
 
-    private void OnTriggerEnter(Collider other){
+    private void OnTriggerEnter(Collider other)
+    {
         if(other.CompareTag("Player")){
             if(openTrigger){
                 myDoor.Play("dooropen", 0, 0.0f);
@@ -20,15 +21,27 @@ public class DoorController : MonoBehaviour
             }
         }
     }
-    // Start is called before the first frame update
-    void Start()
+
+}
+
+
+
+    /*public Animator myDoor = null;
+    public bool openTrigger = false;
+    public bool closeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.CompareTag("Player")){
+            if(openTrigger){
+                myDoor.Play("dooropen", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+            else if(closeTrigger){
+                myDoor.Play("doorclose", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+}*/
